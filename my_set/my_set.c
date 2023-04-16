6830798071ec79ddb4614345ed12055af7b54161
tessag <tessagoldchmit@gmail.com>
2023-04-17 00:13:06 +0300

    (+) added docstring

--- my_set/my_set.c
+++ my_set/my_set.c
@@ -1,13 +1,25 @@
+/*
+ * A set implementation in C using a dynamic array of integers.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Macro to increase the size of an array by sizeof(int) bytes */
 #define ENLARGE_SIZE(size) ((size) + sizeof(int))
 
+/* A struct representing a set of integers */
 struct Set {
-    int *head_ptr;  /* pointer to array of integers (dynamically allocated) */
-    int last_index; /* index to the last cell in the array */
+    int *head_ptr;  /* Pointer to array of integers (dynamically allocated) */
+    int last_index; /* Index to the last cell in the array */
 };
 
+/*
+ * Adds a new integer to the set if it doesn't already exist.
+ *
+ * Parameters:
+ * s: a pointer to a struct Set representing the set to add the integer to.
+ * num: the integer to add to the set.
+ */
 void add_to_set(struct Set *s, int num) {
     int i;
     int *ptr = s->head_ptr;
@@ -22,7 +34,7 @@ void add_to_set(struct Set *s, int num) {
 
     /* Check if the value already exists in the set */
     for (i = 0; i < s->last_index; i++) {
-        if (*(ptr + i) == num) /* means the number is already in the set, and we can exit the function*/
+        if (*(ptr + i) == num) /* means the number is already in the set, and we can exit the function */
             return;
     }
 
@@ -32,6 +44,12 @@ void add_to_set(struct Set *s, int num) {
     s->last_index++;
 }
 
+/*
+ * Prints the contents of a set in the order they were added.
+ *
+ * Parameters:
+ * s: a pointer to a struct Set representing the set to print.
+ */
 void print_set(struct Set *s) {
     int i;
     int *ptr = s->head_ptr;
@@ -39,6 +57,12 @@ void print_set(struct Set *s) {
         printf("%d ", *(ptr + i));
 }
 
+/*
+ * Reads a list of integers from the user and adds them to the set.
+ *
+ * Parameters:
+ * s: a pointer to a struct Set representing the set to add integers to.
+ */
 void get_set(struct Set *s) {
     int num;
     while (scanf("%d ", &num) == 1 && num != EOF) {
